380a5a7ef9a29b5074bbde5ac9aac74703a3334f
naoya.inoue <a.good.for.nothing.fellow@gmail.com>
2018-11-23 08:16:24 +0000

    boostrap edit add.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <body>
         @include('commons.navbar')
         
-        <div class="container>"
+        <div class="container">
             @include('commons.error_messages')
             
             @yield('content')
